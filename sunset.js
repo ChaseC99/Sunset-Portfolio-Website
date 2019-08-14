9017c52a5c79e9868e011762c7b468aa43d83f44
Chase Carnaroli <chasecarnaroli@Chases-Mac.local>
2019-08-14 13:50:43 -0700

    Improved gallery item hover state

--- sunset.js
+++ sunset.js
@@ -4,22 +4,13 @@
     When an image in the gallery is clicked, it is presented full screen.
 **************************/
 
-let selectedCard = null;
-
-function openImg() {
-    selectedCard.addClass('no-hover');
-
-    const url = selectedCard.find("img").attr("src");
+function openImg(imgUrl) {
     $('#fullscreen_overlay').css('display', 'block');
-    $('#fullscreen_overlay').find("img").attr('src', url);
+    $('#fullscreen_overlay').find("img").attr('src', imgUrl);
 }
 
 function closeImg() {
-    if (selectedCard) {
-        selectedCard.removeClass('no-hover');
-        selectedCard = null;
-        $('#fullscreen_overlay').css('display', 'none');
-    }
+    $('#fullscreen_overlay').css('display', 'none');
 }
 
 $(document).ready(function() {
@@ -28,7 +19,7 @@ $(document).ready(function() {
     });
 
     $('#gallery li').click(function(e) {
-        selectedCard = $(this);
-        openImg();
+        const imgUrl = $(this).find("img").attr("src");
+        openImg(imgUrl);
     });
 });
